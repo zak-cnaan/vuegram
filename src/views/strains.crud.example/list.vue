158d76a93550d1e62b07b50f665727e4e92fedb2
zak cnaan <zak@zakcnaan.com>
2020-01-12 14:42:39 +0200

    example

---
+++ src/views/strains.crud.example/list.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+          <v-card class="mb-2" outlined v-for="formData in strains" :key="formData.id"
+    elevation="2">
+
+    <v-card-title>
+        {{formData.name}}
+      </v-card-title>
+      <v-card-text>
+        {{formData.description}}
+      </v-card-text>
+
+
+      <v-card-actions>
+      <v-spacer></v-spacer>
+
+        <v-btn fab dark small color="secondary"
+        :to="{ name: 'Strain - Single', params: { id: formData.id }}">
+      <v-icon dark>mdi-eye</v-icon>
+    </v-btn>
+
+    </v-card-actions>
+
+          
+          </v-card>
+
+          <v-btn
+                  color="secondary"
+                  fab
+                  dark
+                  absolute
+                  bottom
+                  right
+                  :to="{path: '/strains/edit'}"
+                >
+                  <v-icon>mdi-plus</v-icon>
+                </v-btn>
+    </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+    data() {
+    return {
+      
+    };
+  },
+  mounted() {
+    this.$store.dispatch("fetchStrains");
+  },
+  computed: {
+    ...mapState(["strains"])
+  },
+  methods: {
+  }
+}
+</script>
---
+++ src/views/strains.crud.example/single.vue
@@ -0,0 +1,148 @@
+<template>
+  <div>
+    <v-card
+    outlined
+    elevation="2"
+    >
+      <v-card-title>
+        {{formData.name}}
+      </v-card-title>
+      <v-card-text>
+        {{formData.description}}
+
+      </v-card-text>
+
+      <v-card-actions>
+      <v-spacer></v-spacer>
+
+        <v-btn  fab dark small color="secondary"
+        :to="{ name: 'Strains - Edit', params: { id: docId }}">
+      <v-icon dark>mdi-pencil</v-icon>
+    </v-btn>
+
+    <v-btn @click.stop="dialog = true" fab dark small color="error">
+      <v-icon dark>mdi-delete</v-icon>
+    </v-btn>
+
+
+
+    </v-card-actions>
+
+
+    </v-card>
+
+    <v-dialog
+      v-model="dialog"
+      max-width="290"
+    >
+      <v-card dark>
+        <v-card-title class="headline">Delete?</v-card-title>
+
+        <v-card-text>
+          {{formData.name}}
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="error"
+            text
+            @click="dialog = false"
+          >
+            No!
+          </v-btn>
+
+          <v-btn
+            color="success"
+            text
+            @click="handleDelete"
+          >
+            Delete
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  
+  </div>
+</template>
+
+<script>
+const fb = require("@/firebaseConfig.js");
+
+export default {
+  data: () => ({
+      docId: null,
+      dialog: false,
+    formData: {
+
+      name: '',
+      description: '',
+      
+    }
+  }),
+  mounted() {
+   if(this.$route.params.id){
+     this.docId = this.$route.params.id;
+     this.getDocById();
+   };
+  },
+  methods: {
+    handleDelete () {
+      this.dialog = false;
+      this.$store.commit("setLoadingCounter", "ADD");
+
+      
+      var collectionRef = "strainsCollection";
+
+      fb[collectionRef]
+        .doc(this.$route.params.id)
+        .delete()
+        .then(ref => {
+      this.$store.commit("setLoadingCounter", "remove");
+
+          this.$router.push('/strains');
+
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    getDocById: (function () {
+      this.$store.commit("setLoadingCounter", "ADD");
+
+      var collectionRef = "strainsCollection";
+
+      fb[collectionRef]
+        .doc(this.$route.params.id)
+        .get()
+        .then(ref => {
+          //this.post.content = "";
+          if (!ref.exists){
+            this.docId = null;
+            return;
+          } 
+          let data = ref.data();
+          this.formData = data;
+
+      this.$store.commit("setLoadingCounter", "remove");
+
+          // console.log(data.date1)
+          // console.log(this.date1)
+
+
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
+    }),
+      
+  },
+  computed: {
+      // computedDateFormattedMomentjs () {
+      //   return this.date3 ? moment(this.date3).format('ddd, D.M.YYYY') : ''
+      // },
+    },
+};
+</script>
