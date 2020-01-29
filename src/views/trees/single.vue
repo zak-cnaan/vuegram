ec800e8d90fa2213e2d78e76316f8a8b0fe8daf0
Izak Cohen <izak@corp.moonsite.co.il>
2020-01-29 17:01:55 +0200

    single tree germination time

--- src/views/trees/single.vue
+++ src/views/trees/single.vue
@@ -1,22 +1,34 @@
 <template>
   <div>
     <v-card outlined elevation="2">
-      <v-card-title>
-        {{ formData.name }}
-      </v-card-title>
+      <v-card-title>{{ formData.name }}</v-card-title>
       <v-card-text>
-        {{ formData.date1 }}<br />
-        {{ "germinationTime: " + germinationTime + " Days old." }}<br />
-        {{ formData.date2 }}<br />
-        {{ formData.date3 }}<br />
-        {{ formData.date4 }}<br />
-        {{ formData.date5 }}<br />
-        {{ formData.date6 }}<br />
-        {{ formData.roomId }}<br />
-        {{ formData.strainId }}<br />
-        {{ formData.time + " Days" }}<br />
-        {{ stage }}<br />
-        {{ stageName }}<br />
+        {{ formData.date1 }}
+        <br />
+        {{ "germinationTime: " + germinationTime + " Days old." }}
+        <br />
+        {{ formData.date2 }}
+        <br />
+        {{ "veggingTime: " + veggingTime + " Days old." }}
+        <br />
+        {{ formData.date3 }}
+        <br />
+        {{ formData.date4 }}
+        <br />
+        {{ formData.date5 }}
+        <br />
+        {{ formData.date6 }}
+        <br />
+        {{ formData.roomId }}
+        <br />
+        {{ formData.strainId }}
+        <br />
+        {{ formData.time + " Days" }}
+        <br />
+        {{ stage }}
+        <br />
+        {{ stageName }}
+        <br />
       </v-card-text>
 
       <v-card-actions>
@@ -42,20 +54,14 @@
       <v-card dark>
         <v-card-title class="headline">Delete?</v-card-title>
 
-        <v-card-text>
-          {{ formData.name }}
-        </v-card-text>
+        <v-card-text>{{ formData.name }}</v-card-text>
 
         <v-card-actions>
           <v-spacer></v-spacer>
 
-          <v-btn color="error" text @click="dialog = false">
-            No!
-          </v-btn>
+          <v-btn color="error" text @click="dialog = false">No!</v-btn>
 
-          <v-btn color="success" text @click="handleDelete">
-            Delete
-          </v-btn>
+          <v-btn color="success" text @click="handleDelete">Delete</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -64,7 +70,7 @@
 
 <script>
 const fb = require("@/firebaseConfig.js");
-import moment from 'moment'
+import moment from "moment";
 
 export default {
   data: () => ({
@@ -115,12 +121,11 @@ export default {
 
           if (!ref.exists) {
             this.docId = null;
-            alert('this.docId = null');
+            alert("this.docId = null");
             return;
           }
           let data = ref.data();
           this.formData = data;
-
 
           // console.log(data.date1)
           // console.log(this.date1)
@@ -135,26 +140,40 @@ export default {
     // computedDateFormattedMomentjs () {
     //   return this.date3 ? moment(this.date3).format('ddd, D.M.YYYY') : ''
     // },
-    stage (){ // returns 1-6 representing 1-6 stage dates
+    stage() {
+      // returns 1-6 representing 1-6 stage dates
       let i = 1;
       let ret;
-      for (let i = 1; i <= 6;++i){
-        if (this.formData[("date"+i)])
-          ret = i;
+      for (let i = 1; i <= 6; ++i) {
+        if (this.formData["date" + i]) ret = i;
       }
       return ret;
-
     },
-    stageName(){
-      let arr = ["Ger", "Veg", "Flower", "Dry", "Cure", "Done!"]
+    stageName() {
+      let arr = ["Ger", "Veg", "Flower", "Dry", "Cure", "Done!"];
       return arr[this.stage - 1];
     },
-    germinationTime(){
+    germinationTime() {
+      if (!this.formData.date1) return null;
       var currDate = moment(this.formData.date1);
-  var dateToTest = this.formData.date2 ? moment(this.formData.date2):moment.now();
-  // if dateToTest will always be in past, use currDate as the base to diff, else 
-  //be prepared to handle the negative outcomes. 
-  var result = currDate.diff(dateToTest, 'days')
+      var dateToTest = this.formData.date2
+        ? moment(this.formData.date2)
+        : moment.now();
+      // if dateToTest will always be in past, use currDate as the base to diff, else
+      //be prepared to handle the negative outcomes.
+      var result = currDate.diff(dateToTest, "days");
+
+      return -result;
+    },
+    veggingTime() {
+      if (!this.formData.date2) return null;
+      var currDate = moment(this.formData.date2);
+      var dateToTest = this.formData.date3
+        ? moment(this.formData.date3)
+        : moment.now();
+      // if dateToTest will always be in past, use currDate as the base to diff, else
+      //be prepared to handle the negative outcomes.
+      var result = currDate.diff(dateToTest, "days");
 
       return -result;
     }
